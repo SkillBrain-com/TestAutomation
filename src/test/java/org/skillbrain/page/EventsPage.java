350ebdbdbbb29a3ed9d36f8af3bfcdbc84f3f73e
Sandu Cristian Claudiu <sandu_cristian2007@yahoo.com>
2025-08-07 21:00:09 +0300

    adding EventsPage class

---
+++ src/test/java/org/skillbrain/page/EventsPage.java
@@ -0,0 +1,11 @@
+package org.skillbrain.page;
+
+import org.openqa.selenium.WebDriver;
+
+public class EventsPage extends BasePage{
+
+
+    public EventsPage(WebDriver driver) {
+        super(driver);
+    }
+}
