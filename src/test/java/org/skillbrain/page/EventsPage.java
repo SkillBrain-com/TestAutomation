9385d432a59a35081c0a2c103d529b8e42aa3aaa
DanielaMold <moldoveanudaniela25@gmail.com>
2025-08-28 20:37:02 +0300

    my second commit
    # Conflicts:
    #	src/test/java/org/skillbrain/page/PageObjectFactory.java

--- src/test/java/org/skillbrain/page/EventsPage.java
+++ src/test/java/org/skillbrain/page/EventsPage.java
@@ -5,8 +5,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import java.time.Duration;
-
 public class EventsPage extends BasePage{
     private WebDriver driver;
     @FindBy(xpath = "//p[normalize-space()='Events'] //parent::div")
@@ -19,26 +17,13 @@ public class EventsPage extends BasePage{
     public EventsPage(WebDriver driver) {
         super(driver);
         this.driver = driver;
-        this.eventsSidebar = eventsSidebar;
-        this.vouchers = vouchers;
-        this.createVoucher = createVoucher;
         PageFactory.initElements(this.driver, this);
     }
 
-//    public EventsPage(WebDriver driver) {
-//        super(driver);
-//    }
-
     public void clickSidebar(){
-            eventsSidebar.click();
+        eventsSidebar.click();
+
     }
 
-    public void clickVouchers(){
-        waitForText("Vouchers", Duration.ofSeconds(5));
-        vouchers.click();
-    }
-
-    public void clickCreateVoucherButton(){
-        createVoucher.click();
-    }
+    public void
 }
--- src/test/java/org/skillbrain/stepDefinition/EventsPageStepDefinition.java
+++ src/test/java/org/skillbrain/stepDefinition/EventsPageStepDefinition.java
@@ -1,18 +1,9 @@
 package org.skillbrain.stepDefinition;
 
-import io.cucumber.java.en.And;
 import io.cucumber.java.en.When;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 import org.skillbrain.page.AttractionForm;
-import org.skillbrain.page.BasePage;
 import org.skillbrain.page.EventsPage;
 import org.skillbrain.utilities.TestContextSetup;
-import org.testng.Assert;
-
-import java.time.Duration;
 
 public class EventsPageStepDefinition {
 
@@ -24,8 +15,6 @@ public class EventsPageStepDefinition {
         this.testContextSetup = testContextSetup;
         attractionForm = testContextSetup.pageObjectFactory.getAttractionForm();
         eventsPage = testContextSetup.pageObjectFactory.getEventsPage();
-        //voucherPage = testContextSetup.pageObjectFactory.getVoucherPage();
-
     }
 
     @When("I click on Events sidebar")
@@ -33,19 +22,11 @@ public class EventsPageStepDefinition {
         eventsPage.clickSidebar();
     }
 
-   //@And("I click on Vouchers1")
     public void iClickOnVouchers() {
-       attractionForm.waitForText("Vouchers", Duration.ofSeconds(5));
-       eventsPage.clickVouchers();
-
     }
 
- //@And("I click on Create voucher button1")
     public void iClickOnCreateVoucherButton() {
-        attractionForm.waitForText("Create voucher", Duration.ofSeconds(5));
-        eventsPage.clickCreateVoucherButton();
     }
-
 
 
 }
