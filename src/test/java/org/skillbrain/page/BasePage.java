867cf35f206b1462cf332d3db2bf9578004bc66c
mare-adrian-30021 <mareadrianvia2@gmail.com>
2025-08-28 20:21:22 +0300

    fixing conflicts

--- src/test/java/org/skillbrain/page/BasePage.java
+++ src/test/java/org/skillbrain/page/BasePage.java
@@ -26,6 +26,12 @@ public class BasePage {
         driverWait.until(ExpectedConditions.presenceOfElementLocated(xpath));
     }
 
+    public void setWait() {
+        if (driverWait == null) {
+            driverWait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        }
+    }
+
     public void waitForVisibility(WebElement element, Duration duration) {
         WebDriverWait wait = new WebDriverWait(driver, duration);
         wait.until(ExpectedConditions.visibilityOf(element));
@@ -34,4 +40,9 @@ public class BasePage {
     public void scrollToElement(WebElement element) {
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
     }
+
+    public WebDriverWait getDriverWait() {
+        return driverWait;
+    }
 }
+
