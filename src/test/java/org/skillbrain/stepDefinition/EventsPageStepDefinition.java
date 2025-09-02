8810c92223deb9823055f1aa73973c5d3f9d8448
Sandu Cristian Claudiu <sandu_cristian2007@yahoo.com>
2025-09-02 19:09:18 +0300

    fixing conflict

--- src/test/java/org/skillbrain/stepDefinition/EventsPageStepDefinition.java
+++ src/test/java/org/skillbrain/stepDefinition/EventsPageStepDefinition.java
@@ -1,9 +1,18 @@
 package org.skillbrain.stepDefinition;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.skillbrain.page.AttractionForm;
+import org.skillbrain.page.BasePage;
 import org.skillbrain.page.EventsPage;
 import org.skillbrain.utilities.TestContextSetup;
+import org.testng.Assert;
+
+import java.time.Duration;
 
 public class EventsPageStepDefinition {
 
@@ -15,6 +24,8 @@ public class EventsPageStepDefinition {
         this.testContextSetup = testContextSetup;
         attractionForm = testContextSetup.pageObjectFactory.getAttractionForm();
         eventsPage = testContextSetup.pageObjectFactory.getEventsPage();
+        //voucherPage = testContextSetup.pageObjectFactory.getVoucherPage();
+
     }
 
     @When("I click on Events sidebar")
@@ -22,11 +33,19 @@ public class EventsPageStepDefinition {
         eventsPage.clickSidebar();
     }
 
+   //@And("I click on Vouchers1")
     public void iClickOnVouchers() {
+       attractionForm.waitForText("Vouchers", Duration.ofSeconds(5));
+       eventsPage.clickVouchers();
+
     }
 
+ //@And("I click on Create voucher button1")
     public void iClickOnCreateVoucherButton() {
+        attractionForm.waitForText("Create voucher", Duration.ofSeconds(5));
+        eventsPage.clickCreateVoucherButton();
     }
+
 
 
 }
