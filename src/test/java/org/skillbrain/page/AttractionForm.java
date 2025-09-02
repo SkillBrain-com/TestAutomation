72df1daf9db62dc6bf4f88db298d2cc4a78601ed
Sandu Cristian Claudiu <sandu_cristian2007@yahoo.com>
2025-09-02 20:10:50 +0300

    fixing stuff

--- src/test/java/org/skillbrain/page/AttractionForm.java
+++ src/test/java/org/skillbrain/page/AttractionForm.java
@@ -2,21 +2,14 @@ package org.skillbrain.page;
 
 
 import org.openqa.selenium.Keys;
-
-import org.bouncycastle.cms.PasswordRecipient;
-import org.openqa.selenium.By;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 import java.time.Duration;
-import java.time.Instant;
 //TODO MERGE DUPLICATE SELECTORS
 public class AttractionForm extends BasePage{
 
